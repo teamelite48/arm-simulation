ab9fba3880cda3dbe29ac3afe45d63a63be63dd5
Matthew Jones <mtjones330@fastmail.com>
2023-02-20 19:48:00 -0500

    Scale to robot dimensions

--- arm-simulation.js
+++ arm-simulation.js
@@ -19,10 +19,18 @@ const effector = {
 	yVel: 0
 }
 
+const scale = 100 / 1;
+
 const speed = 2;
-const armLength1 = 112;
-const armLength2 = 112;
-const radius = armLength1 + armLength2;
+const a1 =  0.9525 * scale;
+const a2 = 0.81838079 * scale;
+
+const a3 = 0.22609618 * scale; // TODO: get real value
+
+const baseLength = 0.8810244 * scale;
+const baseHeightFromGround = 0.18016220 * scale; // TODO: get real value
+
+const maxRadius = a1 + a2;
 
 let theta1 = 0;
 let theta2 = 0;
@@ -98,17 +106,17 @@ function updatePosition() {
 
 function isMovementAllowed(x, y) {
 
-	if (y < 0) {
+	if (y < -baseHeightFromGround) {
 		return false;
 	}
 
-	const yPosUpperBound = Math.sqrt(Math.pow(radius, 2) - Math.pow(x, 2));
+	const yPosUpperBound = Math.sqrt(Math.pow(maxRadius, 2) - Math.pow(x, 2));
 
 	if (y > yPosUpperBound) {
 		return false;
 	}
 
-	const xPosUpperBound = Math.sqrt(Math.pow(radius, 2) - Math.pow(y, 2));
+	const xPosUpperBound = Math.sqrt(Math.pow(maxRadius, 2) - Math.pow(y, 2));
 
 	if (x > xPosUpperBound) {
 		return false;
@@ -120,6 +128,7 @@ function isMovementAllowed(x, y) {
 function draw() {
 	ctx.clearRect(-(canvasWidth / 2), -(canvasHeight / 2), canvasWidth, canvasHeight);
 	drawSpace()
+	drawBase();
 	drawArm();
 }
 
@@ -141,41 +150,34 @@ function drawSpace() {
 	ctx.lineWidth = 1;
 
 	ctx.beginPath()
-	ctx.arc(0, 0, radius, 0, degreesToRadians(360), true);
+	ctx.arc(0, 0, maxRadius, 0, degreesToRadians(360), true);
 	ctx.stroke();
 
 	ctx.lineWidth = 5;
 
 }
 
-function drawArm() {
-	drawEffector()
-	const arm1EndPoint = drawArmLength1();
-	drawArmLength2(arm1EndPoint);
-}
+function drawBase() {
 
-function drawEffector() {
-
-	const endPoint = findEndPoint(effector.xPos, effector.yPos, theta1 + theta2 + theta3, 32)
+	ctx.lineWidth = baseHeightFromGround;
 
 	ctx.beginPath();
-	ctx.moveTo(effector.xPos, effector.yPos);
-	ctx.lineTo(endPoint.x, endPoint.y);
-	ctx.strokeStyle = 'red';
+	ctx.moveTo(-baseLength/2, -baseHeightFromGround/2);
+	ctx.lineTo(baseLength/2, -baseHeightFromGround/2);
+	ctx.strokeStyle = 'gray';
 	ctx.stroke();
 }
 
-function findEndPoint(x0, y0, radians, length) {
-
-  var x = x0 + length * Math.cos(radians);
-  var y = y0 + length * Math.sin(radians);
-
-  return {x: x, y: y};
+function drawArm() {
+	ctx.lineWidth = 5;
+	const arm1EndPoint = drawArmLength1();
+	drawArmLength2(arm1EndPoint);
+	drawEffector()
 }
 
 function drawArmLength1() {
 
-	const endPoint = findEndPoint(0, 0, theta1, armLength1);
+	const endPoint = findEndPoint(0, 0, theta1, a1);
 
 	ctx.beginPath();
 	ctx.moveTo(0, 0);
@@ -188,13 +190,40 @@ function drawArmLength1() {
 
 function drawArmLength2(startPoint) {
 
-	const endPoint = findEndPoint(startPoint.x, startPoint.y, theta1 + theta2, armLength1);
+	const endPoint = findEndPoint(startPoint.x, startPoint.y, theta1 + theta2, a2);
 
 	ctx.beginPath();
 	ctx.moveTo(startPoint.x, startPoint.y);
 	ctx.lineTo(endPoint.x, endPoint.y);
 	ctx.strokeStyle = 'green';
 	ctx.stroke();
+}
+
+function drawEffector() {
+
+	const endPoint1 = findEndPoint(effector.xPos, effector.yPos, theta1 + theta2 + theta3, a3 / 2)
+
+	ctx.beginPath();
+	ctx.moveTo(effector.xPos, effector.yPos);
+	ctx.lineTo(endPoint1.x, endPoint1.y);
+	ctx.strokeStyle = 'red';
+	ctx.stroke();
+
+	const endPoint2 = findEndPoint(effector.xPos, effector.yPos, theta1 + theta2 + theta3, -a3 / 2)
+
+	ctx.beginPath();
+	ctx.moveTo(effector.xPos, effector.yPos);
+	ctx.lineTo(endPoint2.x, endPoint2.y);
+	ctx.strokeStyle = 'red';
+	ctx.stroke();
+}
+
+function findEndPoint(x0, y0, radians, length) {
+
+  var x = x0 + length * Math.cos(radians);
+  var y = y0 + length * Math.sin(radians);
+
+  return {x: x, y: y};
 }
 
 function degreesToRadians(degrees) {
@@ -206,17 +235,23 @@ function calculateThetas() {
 	const x = effector.xPos;
 	const y = effector.yPos;
 
-	const a = armLength1;
-	const b = armLength2;
+	const a = a1;
+	const b = a2;
 
 	const r = Math.sqrt(x**2 + y**2);
 
-	const theta = lawOfCosines(y, r, x);
-	const alpha = lawOfCosines(b, a, r);
-	theta1 = theta + alpha;
+	const alpha = lawOfCosines(y, r, x);
+	const beta = lawOfCosines(b, a, r);
 
-	const beta = lawOfCosines(r, a, b);
-	theta2 = -Math.PI + beta;
+	if (y >= 0) {
+		theta1 = alpha + beta;
+	}
+	else {
+		theta1 = -alpha + beta;
+	}
+
+	const gamma = lawOfCosines(r, a, b);
+	theta2 = -Math.PI + gamma;
 
 	theta3 = -theta2 - theta1 + degreesToRadians(45);
 }
